15157b9eb4471aa4e459fb67e52041db855a6973
McCabe <mccabe@wambl.com>
2017-09-05 14:53:08 -0700

    adding

---
+++ compareLists.py
@@ -0,0 +1,9 @@
+arr1 = []
+arr1 = raw_input("please enter list1:")
+arr2 = []
+arr2 = raw_input("please enter list2:")
+
+if arr1 == arr2:
+    print "arrays are the same"
+else:
+    print "arrays are not the same"
